8ae654d08791d8fb1c1659064ed85f7c3a194f6f
Birgit <birgitkulli@gmail.com>
2021-01-02 17:03:56 +0200

    Added new information

---
+++ src/Temperature.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Temperature.css";
+
+export default function Temperature() {
+  return (
+    <div className="Temperature">
+      <h1>
+        <img src={require("../images/01d.png")} alt="sun" />
+        -2°C
+        <span className="units">
+          <a href="#" id="scale">
+            /°F
+          </a>
+        </span>
+      </h1>
+    </div>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Weather.css";
+import City from "./City";
+import Temperature from "./Temperature";
+
+export default function Weather() {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="row"></div>
+          <div className="row">
+            <div className="col-8">
+              <div className="col p-0">
+                <City />
+              </div>
+              <div className="col p-0">
+              </div>
+            </div>
+            <div className="col-4 p-0">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
